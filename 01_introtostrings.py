7b7983464afb598a5ed07889395ebac688699950
MuhammadTheRuralGeek <mohammadrashim99@gmail.com>
2025-03-25 22:14:56 +0530

    Update 01_introtostrings.py

--- 01_introtostrings.py
+++ 01_introtostrings.py
@@ -1,6 +1,6 @@
-name="Rashim"
-name1='Rashim'
-name2='''Rashlp'''
+name="Adithya"
+name1='Adithya'
+name2='''Adithyap'''
 
 nameshort=name[0:3] #include 0th index exclude 3rd index i.e. 0,1,2
 print(nameshort)
